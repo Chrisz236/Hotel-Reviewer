4d8ea87790e43561a763feebf01e713eb211c43f
Chrisz236 <44371223+Chrisz236@users.noreply.github.com>
2023-08-15 10:51:11 -0700

    Create API end-point for query

--- app.py
+++ app.py
@@ -1,7 +1,54 @@
 # Author:  Haolin Zhang
 # Email:   zhanghaolin66@gmail.com
-# File:    server.py
-# Date:    May 30, 2023
+# File:    app.py
+# Date:    Aug 14, 2023
 # Version: 1.0
 
+import os
+import openai
 from flask import Flask, request
+from scraper import get_reviews_by_name
+
+app = Flask(__name__)
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+@app.route('/reviews', methods=['GET'])
+def summary():
+    hotel_name = request.args.get('hotel', '')
+    reviews = get_reviews_by_name(hotel_name)
+    return review_summary(reviews)
+
+def review_summary(reviews):
+    """Summarize reviews from TripAdvisor.com
+
+        Parameters
+        ----------
+        reviews : str
+            latest 5 reviews of hotel
+        
+        Returns
+        -------
+        summarized_reviews : str
+            single paragraph of summary
+    """
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        temperature=0,
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a highly skilled AI trained in language comprehension and summarization. I would like you to read the following text and summarize it into a concise abstract paragraph. Aim to retain the most important points, providing a coherent and readable summary that could help a person understand the main points of the discussion without needing to read the entire text. Please avoid unnecessary details or tangential points."
+            },
+            {
+                "role": "user",
+                "content": reviews
+            }
+        ]
+    )
+    return response['choices'][0]['message']['content']
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
+# Homewood%20Suites%20by%20Hilton%20Newark-Fremont
