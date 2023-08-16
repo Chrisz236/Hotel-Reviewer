cca7735b973ce4f8af1915a14c796439d2ace12f
Chrisz236 <44371223+Chrisz236@users.noreply.github.com>
2023-08-16 14:24:50 -0700

    fuzzy match address

--- scraper.py
+++ scraper.py
@@ -9,11 +9,12 @@ import os
 import requests
 from urllib.parse import quote, unquote
 from bs4 import BeautifulSoup
+from rapidfuzz import fuzz
 
 # developing only
 TRIP_ARVISOR_API_KEY = os.getenv("TRIP_ADVISOR_API_KEY")
 
-def get_location_id_by_name_and_address(name, address):
+def get_location_id_by_name_address_city(name, address, city):
     """Get location_id of given hotel name
 
         Parameters
@@ -29,16 +30,31 @@ def get_location_id_by_name_and_address(name, address):
     hotel_name = quote(name)
     address = quote(unquote(address).split(',', 1)[0])
 
-    url = f"https://api.content.tripadvisor.com/api/v1/location/search?key={TRIP_ARVISOR_API_KEY}&searchQuery={hotel_name}&category=hotels&address={address}&language=en"
+    url = f"https://api.content.tripadvisor.com/api/v1/location/search?key={TRIP_ARVISOR_API_KEY}&searchQuery={hotel_name}&category=hotels&language=en"
     headers = {"accept": "application/json"}
 
     response = requests.get(url, headers=headers)
     json_data = response.json()
 
-    return str(json_data["data"][0]["location_id"])
+    # This JSON contains up to 10 results from the query
+    # Iterate list of JSON and fuzzy match the hotel that matches our address
+
+    location_id = ""
+    address_match_score = 0
+
+    for i in range(len(json_data["data"])):
+        # quick filter for city match
+        if json_data["data"][i]["address_obj"]["city"].lower() == city.lower():
+
+            similarity = fuzz.ratio(json_data["data"][i]["address_obj"]["address_string"].lower(), address.lower())
+            if similarity > address_match_score:
+                location_id = json_data["data"][i]["location_id"]
+                address_match_score = similarity
+
+    return location_id
 
 
-def get_reviews_by_name_and_address(hotel_name, address):
+def get_reviews_by_name_address_city(hotel_name, address):
     """Get reviews by given hotel name
 
         Parameters
@@ -51,7 +67,7 @@ def get_reviews_by_name_and_address(hotel_name, address):
         reviews : str
             latest reviews for given hotel name
     """
-    location_id = get_location_id_by_name_and_address(hotel_name, address)
+    location_id = get_location_id_by_name_address_city(hotel_name, address, city)
     url = f"https://www.tripadvisor.com/Hotel_Review-d{location_id}.html"
     headers = {'User-Agent': 'Mozilla/5.0'}
 
@@ -66,3 +82,6 @@ def get_reviews_by_name_and_address(hotel_name, address):
     ][:5]
 
     return "\n\n".join(reviews)
+
+if __name__ == '__main__':
+    print(get_location_id_by_name_address_city("Bellagio Hotel &amp; Casino", "3600 Las Vegas Blvd S, Las VegasNevada 89109, Las Vegas, 89109, USA", "Las Vegas"))
